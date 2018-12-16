de56cae576e00d996fa0fca8b9968653c4a271b2
Eugenia Glinska <willow@nyan.org.ua>
2018-12-16 19:10:58 +0200

    Delete unnecessary check and add second test

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ from file_parser import FileParser
 
 
 parser = FileParser("tests/test_one.txt")
-exit_code = parser.get_initials()
-if exit_code == 0:
-    exit(0)
+parser.get_initials()
+city = parser.get_city()
+city.print_layout()
+queries = parser.get_queries()
+coffee_finder = CoffeeFinder(city)
+for query in queries:
+    optimum = coffee_finder.find_coffee(int(query))
+    for coffee_number, cell in optimum.items():
+        print("Found {0} coffeeshops near this cell: {1}".format(coffee_number, cell))
+
+print("\n")
+parser = FileParser("tests/test_two.txt")
+parser.get_initials()
 city = parser.get_city()
 city.print_layout()
 queries = parser.get_queries()
